abbd59532a0f5382987cd8806c305436ad7308ca
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2026-02-06 20:37:57 -0800

    updated for VAST 4.0.0

--- scripts/ms slope/r4bis_fit_OM_slope.R
+++ scripts/ms slope/r4bis_fit_OM_slope.R
@@ -44,29 +44,35 @@ n_sim <- 100
 ##  Fit VAST models
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-for (ispp in 1:(nrow(x = species_list) - 1)) {
+for (ispp in nrow(x = species_list)-1 ) {
+  # for (ispp in 1:nrow(x = species_list)) {
+  species_name <- species_list$SCIENTIFIC_NAME[ispp]
   for (iregion in c("bs_slope", "bs_shelf")[2]) {
+    
+    ## Skip Bering slope model run if it's not included in the slope analysis 
+    if (iregion == "bs_slope" & species_list$SLOPE[ispp]) next
     
     ## Filter data_geostat to region
     data_geostat <-
       readRDS(file = paste0("data/data_processed/species/", 
-                            species_list$SCIENTIFIC_NAME[ispp], 
+                            species_name, 
                             "/data_geostat.rds")) |>
       subset(subset = survey %in% list("bs_slope" = "BSS",
                                        "bs_shelf" = c("EBS", "NBS"))[[iregion]],
              select = c(lat_start, lon_start, year, scientific_name, 
                         weight_kg, area_swept_km2, depth_m, Temp, survey, cpue_kgkm2 ))
     names(x = data_geostat) <- c("Lat", "Lon", "Year", "Species", "Weight_kg",
-                                 "Effort", "Depth", "Temp", "Region", "CPUEkgkm")
+                                 "Area_km2", "Depth", "Temp", "Region", "CPUEkgkm")
     
-    ## Filter interpolation grid to region
+    ## Filter interpolation grid to region. The Bering slope region grid only
+    ## runs from 2002-2016. 
     interpolation_grid <- 
       subset(x = grid_bs, 
              subset = region %in% list("bs_slope" = "BSS",
                                        "bs_shelf" = c("EBS", "NBS"))[[iregion]] 
              & Year %in% list(
                "bs_slope" = 2002:2016,
-               "bs_shelf" = seq(from = ifelse(test = species_list$SCIENTIFIC_NAME[ispp]
+               "bs_shelf" = seq(from = ifelse(test = species_name
                                               %in% c("Atheresthes stomias", 
                                                      "Atheresthes evermanni"),
                                               yes = 1992,
@@ -75,18 +81,18 @@ for (ispp in 1:(nrow(x = species_list) - 1)) {
              )[[iregion]] 
       )
     
-    n_interpolation_cells <- table(interpolation_grid$Year) |> unique()
-    
-    # grid with info to get prediction
+    ## For VAST to provide prediction estimates for all grid cells and years
+    ## we rbind the observed catch and effort dataset (data_geostat) with 
+    ## the interpolation_grid
     data_geostat1 <- rbind(
-      data_geostat[, c("Lat", "Lon", "Year", "Species", "Weight_kg", "Effort",
+      data_geostat[, c("Lat", "Lon", "Year", "Species", "Weight_kg", "Area_km2",
                        "Depth", "Temp", "Region", "CPUEkgkm")],
       data.frame(Lat = interpolation_grid$Lat,
                  Lon = interpolation_grid$Lon,
                  Year = interpolation_grid$Year,
-                 Species = species_list$SCIENTIFIC_NAME[ispp],
+                 Species = species_name,
                  Weight_kg = mean(data_geostat$Weight_kg),
-                 Effort = interpolation_grid$Area_in_survey_km2,
+                 Area_km2 = interpolation_grid$Area_in_survey_km2,
                  Depth = interpolation_grid$depth_m,
                  Temp = interpolation_grid$Temp,
                  Region = interpolation_grid$region,
@@ -107,26 +113,25 @@ for (ispp in 1:(nrow(x = species_list) - 1)) {
     ##  Species/Region specific VAST settings to help with convergence issues
     ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     
+    ## Number of spatial knots for estimation
+    knots <- list("bs_shelf" = 500, "bs_slope" = 300)[[iregion]]
+    
     ## Anisotropy
-    aniso <- ifelse(
-      test = species_list$SCIENTIFIC_NAME[ispp] %in% c("Anoplopoma fimbria",
-                                                       "Atheresthes stomias") 
-      & iregion == "bs_slope",
-      yes = FALSE, 
-      no = TRUE
-    )
+    aniso <- ifelse(test = species_name %in% c("Anoplopoma fimbria", 
+                                               "Atheresthes stomias") 
+                    & iregion == "bs_slope",
+                    yes = FALSE, 
+                    no = TRUE)
     
     ## Newton Steps
-    steps <- ifelse(
-      test = species_list$SCIENTIFIC_NAME[ispp] %in% c("Hippoglossoides elassodon",
-                                                       "Sebastolobus alascanus")
-      & iregion == "bs_slope",
-      yes = 5, 
-      no = 1
-    )
+    steps <- ifelse(test = species_name %in% c("Hippoglossoides elassodon",
+                                               "Sebastolobus alascanus")  
+                    & iregion == "bs_slope",
+                    yes = 5, 
+                    no = 1)
     
     ## Spatial and Spatiotemporal Field Configurations
-    if (species_list$SCIENTIFIC_NAME[ispp] %in% 
+    if (species_name %in% 
         c("Reinhardtius hippoglossoides", "Bathyraja aleutica",
           "Hippoglossoides elassodon", "Sebastes alutus",
           "Sebastolobus alascanus") 
@@ -144,9 +149,7 @@ for (ispp in 1:(nrow(x = species_list) - 1)) {
                                               "Epsilon_year"),
                                             c("Component_1", "Component_2")))
     }
-    
-    ## Number of Spatial knots for estimation
-    knots <- list("bs_shelf" = 500, "bs_slope" = 300)[[iregion]]
+
     
     ## Rho configurations
     rhoconfig <- c("Beta1" = 2, "Beta2" = 2,
@@ -157,7 +160,7 @@ for (ispp in 1:(nrow(x = species_list) - 1)) {
     
     ## Error Distributions, number of knots, and 
     # different specifications that aided convergence in prior runs(?)
-    if (species_list$SCIENTIFIC_NAME[ispp] %in% 
+    if (species_name %in% 
         c("Sebastes melanostictus","Sebastes alutus",
           "Bathyraja aleutica","Sebastolobus alascanus") &
         iregion == "bs_shelf"
@@ -176,7 +179,7 @@ for (ispp in 1:(nrow(x = species_list) - 1)) {
     ## Set up VAST model settings
     settings <- 
       make_settings(n_x = knots,
-                    Region = region,
+                    Region = "User",
                     purpose = "index2",
                     bias.correct = FALSE,
                     knot_method = "grid",
@@ -199,8 +202,9 @@ for (ispp in 1:(nrow(x = species_list) - 1)) {
       )
     if (iregion == "bs_slope")
       covariate_data$Year <- NA
-    formula <- list("bs_shelf" = ' ~ bs(Temp, degree=3, intercept=FALSE)',
-                    "bs_slope" = " ~ bs(Depth, degree=2, intercept=FALSE)")[[iregion]]
+    formula <- 
+      list("bs_shelf" = " ~ bs(Temp, degree=3, intercept=FALSE)",
+           "bs_slope" = " ~ bs(Depth, degree=2, intercept=FALSE)")[[iregion]]
     X1config_cp <- array(c(1, 1), dim = c(1, 1))
     X2config_cp <- X1config_cp
     
@@ -209,75 +213,75 @@ for (ispp in 1:(nrow(x = species_list) - 1)) {
     pred_TF <- rep(1, nrow(x = data_geostat1))
     pred_TF[1:nrow(x = data_geostat)] <- 0
     
-    # fit
-    fit <- tryCatch({
-      fit_model(settings = settings,
-                Lat_i = data_geostat1$Lat,
-                Lon_i = data_geostat1$Lon,
-                t_i = data_geostat1$Year,
-                b_i = data_geostat1$Weight_kg,
-                c_iz = as.numeric(x = factor(x = data_geostat1$Species)) - 1,
-                a_i = data_geostat1$Effort,
-                input_grid = data_geostat1[pred_TF == 1, ],
-                getJointPrecision = TRUE,
-                test_fit = FALSE,
-                covariate_data = covariate_data,
-                X1_formula = formula,
-                X2_formula = formula,
-                newtonsteps = steps,
-                PredTF_i = pred_TF,
-                working_dir = paste0("output/", iregion, "/vast/",
-                                     species_list$SCIENTIFIC_NAME[ispp], "/"))
-    }, error = function(cond) {
-      message("Did not converge.")
-      return(NULL)
-    })
-
+    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ##  Fit model
+    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    fit <-
+      VAST::fit_model(settings = settings,
+                      Lat_i = data_geostat1$Lat,
+                      Lon_i = data_geostat1$Lon,
+                      t_i = data_geostat1$Year,
+                      b_i = data_geostat1$Weight_kg,
+                      a_i = data_geostat1$Area_km2,
+                      input_grid = data_geostat1[pred_TF == 1, ],
+                      getJointPrecision = TRUE,
+                      test_fit = FALSE,
+                      covariate_data = covariate_data,
+                      X1_formula = formula,
+                      X2_formula = formula,
+                      newtonsteps = steps,
+                      PredTF_i = pred_TF,
+                      working_dir = paste0("output/", iregion, "/vast/",
+                                           species_name, "/"))
+    
+    ## Save Fit
+    saveRDS(object = fit, file = paste0("output/", iregion, "/vast/",
+                                        species_name, "/fit.RDS"))
   }
 }
+
+
+
+
+
+# simulate historical data
+n_interpolation_cells <- unique(x = table(grid_bss$Year))
+temp_sim_data <- array(NA,
+                       dim = c(n_interpolation_cells,
+                               length(x = unique(data_geostat1$Year)), 
+                               n_sim),
+                       dimnames = list(NULL,
+                                       sort(x = unique(data_geostat1$Year)),
+                                       NULL)  )
+
+if (inherits(fit, "fit_model")) {
+  save(list = "fit",
+       file = paste(out_dir, fol_region, sp, "fit.RData", sep = "/"))
   
-  
-  
-  
-  
-  # simulate historical data
-  n_interpolation_cells <- unique(x = table(grid_bss$Year))
-  temp_sim_data <- array(NA,
-                         dim = c(n_interpolation_cells,
-                                 length(x = unique(data_geostat1$Year)), 
-                                 n_sim),
-                         dimnames = list(NULL,
-                                         sort(x = unique(data_geostat1$Year)),
-                                         NULL)  )
-  
-  if (inherits(fit, "fit_model")) {
-    save(list = "fit",
-         file = paste(out_dir, fol_region, sp, "fit.RData", sep = "/"))
+  ## Create Simulated Densities
+  for (isim in 1:n_sim) {
+    cat(paste(" ############# Simulation", isim, "of", n_sim, "\n"))
+    Sim1 <- FishStatsUtils::simulate_data(fit = fit, 
+                                          type = 1,
+                                          random_seed = isim)
     
-    ## Create Simulated Densities
-    for (isim in 1:n_sim) {
-      cat(paste(" ############# Simulation", isim, "of", n_sim, "\n"))
-      Sim1 <- FishStatsUtils::simulate_data(fit = fit, 
-                                            type = 1,
-                                            random_seed = isim)
-      
-      
-      temp_sim_data[, , isim] <- 
-        matrix(data = Sim1$b_i[pred_TF == 1] / grid_bss$Area_in_survey_km2,
-               nrow = n_interpolation_cells,
-               ncol = length(x = unique(x = data_geostat1$Year)))  
-      
-    }
+    
+    temp_sim_data[, , isim] <- 
+      matrix(data = Sim1$b_i[pred_TF == 1] / grid_bss$Area_in_survey_km2,
+             nrow = n_interpolation_cells,
+             ncol = length(x = unique(x = data_geostat1$Year)))  
+    
   }
-  
-  if (!dir.exists(paths = paste0("output/species/", sp, 
-                                 "/simulated_historical_data/")))
-    dir.create(path = paste0("output/species/", sp, 
-                             "/simulated_historical_data/"),
-               recursive = TRUE)
-  save(temp_sim_data,
-       file = paste0("output/species/", sp,
-                     "/simulated_historical_data/sim_dens_slope.RData"))
+}
+
+if (!dir.exists(paths = paste0("output/species/", sp, 
+                               "/simulated_historical_data/")))
+  dir.create(path = paste0("output/species/", sp, 
+                           "/simulated_historical_data/"),
+             recursive = TRUE)
+save(temp_sim_data,
+     file = paste0("output/species/", sp,
+                   "/simulated_historical_data/sim_dens_slope.RData"))
 }
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
